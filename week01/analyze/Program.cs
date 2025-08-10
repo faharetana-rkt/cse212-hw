89ac47a8fe3fb448d072502c63483840a7872921
faharetana-rkt <rakotomaharitra@gmail.com>
2025-08-10 11:50:02 +0300

    finished bst

--- week01/analyze/Program.cs
+++ week01/analyze/Program.cs
@@ -6,3 +6,33 @@ StandardDeviation.Run();
 
 Console.WriteLine("\n======================\nSearch\n======================");
 Search.Run();
+
+  // var middle = (sortedNumbers.Length / 2);
+
+        // if (sortedNumbers.Length == 0)
+        // {
+        //     return;
+        // }
+        // if (sortedNumbers.Length == 1)
+        // {
+        //     bst.Insert(sortedNumbers[0]);
+        //     return;
+        // }
+        // if (bst.GetHeight() == 0)
+        // {
+        //     bst.Insert(sortedNumbers[middle]);
+        //     var newsorted = sortedNumbers.Where((_, i) => i != middle).ToArray();
+        //     InsertMiddle(newsorted, first, last, bst);
+        // }
+        // if (bst.GetHeight() > 0 && sortedNumbers[middle] > sortedNumbers[first])
+        // {
+        //     bst.Insert(sortedNumbers[first]);
+        //     var newsorted = sortedNumbers.Where((_, i) => i != first).ToArray();
+        //     InsertMiddle(newsorted, first, last, bst);
+        // }
+        // if (bst.GetHeight() > 0 && sortedNumbers[middle] < sortedNumbers[last])
+        // {
+        //     bst.Insert(sortedNumbers[last - 1]);
+        //     var newsorted = sortedNumbers.Where((_, i) => i != last - 1).ToArray();
+        //     InsertMiddle(newsorted, first, last, bst);
+        // }
--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,3 +1,8 @@
+using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
+using System.Net;
+using System.Runtime.InteropServices.Marshalling;
+
 public class Node
 {
     public int Data { get; set; }
@@ -9,37 +14,93 @@ public class Node
         this.Data = data;
     }
 
+    // This set was moved outside the function insert because it was reset each time the function was called
+    public HashSet<int> unique = new HashSet<int>();
     public void Insert(int value)
     {
         // TODO Start Problem 1
-
-        if (value < Data)
+        if (!unique.Contains(value))
         {
-            // Insert to the left
-            if (Left is null)
-                Left = new Node(value);
+            unique.Add(value);
+            if (value < Data)
+            {
+                // Insert to the left
+                if (Left is null)
+                    Left = new Node(value);
+                else
+                    Left.Insert(value);
+            }
             else
-                Left.Insert(value);
+            {
+                // Insert to the right
+                if (Right is null)
+                    Right = new Node(value);
+                else
+                    Right.Insert(value);
+            }
         }
         else
         {
-            // Insert to the right
-            if (Right is null)
-                Right = new Node(value);
-            else
-                Right.Insert(value);
+            return;
         }
     }
 
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+        if (value == Data)
+        {
+            return true;
+        }
+        else
+        {
+            if (value < Data)
+            {
+                if (Left == null)
+                {
+                    return false;
+                }
+                return Left.Contains(value);
+            }
+            else
+            {
+                if (Right == null)
+                {
+                    return false;
+                }
+                return Right.Contains(value);
+            }
+        }
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        if (Left == null && Right == null)
+        {
+            return 1;
+        }
+
+        int left;
+        if (Left != null)
+        {
+            left = Left.GetHeight();
+        }
+        else
+        {
+            left = 0;
+        }
+
+        int right;
+        if (Right != null)
+        {
+            right = Right.GetHeight();
+        }
+        else
+        {
+            right = 0;
+        }
+
+        return 1 + Math.Max(left, right); // Replace this line with the correct return statement(s)
     }
 }
